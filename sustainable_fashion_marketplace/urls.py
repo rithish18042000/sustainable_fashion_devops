c128ee3fd00d5052d71a8410ec0478d2196fa13c
rithwik <rithwiksr2@gmail.com>
2025-04-05 18:16:18 +0530

    Update urls.py

--- sustainable_fashion_marketplace/urls.py
+++ sustainable_fashion_marketplace/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', 
-                                                  http_method_names=['get', 'post']), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html',
+http_method_names=['get', 'post']), name='logout'),
     path('admin-panel/', include('admin_panel.urls')),
 ]
 
